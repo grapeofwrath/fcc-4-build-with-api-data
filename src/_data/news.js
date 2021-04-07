5904f119bb740a43fa3ae65098b4254248115994
grapeofwrath <grapeofwrath@hey.com>
2021-04-07 14:48:58 -0700

    descriptive env var

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 module.exports = async function() {
   try {
-    const response = await axios.get(`https://newsapi.org/v2/everything?q=Apple&from=2021-04-07&sortBy=popularity&apiKey=${process.env.API_KEY}&pageSize=5`);
+    const response = await axios.get(`https://newsapi.org/v2/everything?q=Apple&from=2021-04-07&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
     return response.data;
   } catch (error) {
     console.error(error);
